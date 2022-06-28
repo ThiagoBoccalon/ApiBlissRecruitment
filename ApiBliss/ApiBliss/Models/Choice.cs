24dbdd1cd8accaafe23181af9717792c48cf1690
ThiagoBoccalon <thiago_rodrigo90@hotmail.com>
2022-06-28 20:57:25 +0100

    Creating domains' classes of API.

---
+++ ApiBliss/ApiBliss/Models/Choice.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ApiBliss.Models;
+
+public class Choice
+{
+    public int Id { get; set; }
+
+    [Required(ErrorMessage = "{0} requied")]
+    public string? OptionOfChoice { get; set; }
+    public int Votes { get; set; }
+    public int QuestionId { get; set; }
+}
---
+++ ApiBliss/ApiBliss/Models/Question.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ApiBliss.Models;
+
+public class Question
+{
+    public int Id { get; set; }
+
+    [Required(ErrorMessage = "{0} requied")]
+    [StringLength(140, MinimumLength = 3, ErrorMessage = "{0} size should be between {2} amd {1}")]
+    public string? Ask { get; set; }
+    public string? ImageUrl { get; set; }
+    public string? ThumbUrl { get; set; }
+
+    [Required(ErrorMessage = "{0} requied")]
+    [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}")]
+    public DateTime PublishedAt { get; set; }
+    
+}
